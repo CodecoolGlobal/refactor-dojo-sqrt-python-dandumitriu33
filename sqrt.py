0048ed9c6bd70eef892de9cc978ebc421905a88e
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-13 10:57:09 +0200

    Add comments with clear description of what blocks do

--- sqrt.py
+++ sqrt.py
@@ -1,4 +1,8 @@
-import random
+# This program reads the lines in a file, adds all the numbers in the line,
+# calculates the square root of the sum of the line and at the end, adds all
+# the square roots.
+
+# import random  # is not used
 import math
 
 sum_of_square_roots_of_lines = 0
@@ -8,6 +12,7 @@ with open('100.txt') as f:
         constructed_number_in_line = ''
         for character in line:
             line_no_spaces = line.strip()
+            # looking through the string and concatenating characters until a "," is reached
             if character != ',':
                 constructed_number_in_line += character
             else:
@@ -16,11 +21,15 @@ with open('100.txt') as f:
                 else:
                     dictionary_of_number_sequences[line_no_spaces] += int(constructed_number_in_line)
                 constructed_number_in_line = ''
+        # in case the ',' character does not exist in a line or is not found
+        # until the end (for example for the last number)
+        # the code moves to this if block
         if line_no_spaces not in dictionary_of_number_sequences:
             dictionary_of_number_sequences[line_no_spaces] = int(constructed_number_in_line)
         else:
             dictionary_of_number_sequences[line_no_spaces] += int(constructed_number_in_line)
 
+# changed the block of code to a for loop of the dictionary
 
 for i in dictionary_of_number_sequences:
     sum_of_square_roots_of_lines += math.sqrt(dictionary_of_number_sequences[i])
